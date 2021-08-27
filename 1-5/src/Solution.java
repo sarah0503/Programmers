c7f6abef529aaf5ef1d82d4d6f3b2fb51c774dfe
Sarah <ysarah0503@gmail.com>
2021-08-28 00:19:02 +0900

    [UPDATE]1-5
    크레인 인형뽑기 게임

--- 1-5/src/Solution.java
+++ 1-5/src/Solution.java
@@ -3,49 +3,44 @@ import java.util.Stack;
 class Solution {
 	public int solution(int[][] board, int[] moves) {
 		int answer = 0;
-		//@SuppressWarnings("unchecked")
-		Stack<Integer>[] a = new Stack[5];
-		for (int i = 0; i < 5; i++) {
-			a[i] = new Stack<Integer>();
-		}
-		Stack<Integer> b = new Stack<>();
-		for (int i = 4; i >= 0; i--) {
-			for (int j = 0; j < 5; j++) {
-				a[j].push(board[i][j]);
-			}
-		}
-		
-		int length = moves.length;
-		for (int i = 0; i < length; i++) {
+		Stack<Integer> b = new Stack<Integer>();
+		int now = 0;
+		for (int i = 0; i < moves.length; i++) {
+			
+			now = -1;
 			int num = moves[i] - 1;
-			int now = 0;
-			while (true) {
-				if (a[num].isEmpty() == true)
+			try {
+			for (int j = 0; j < (board[j].length) ; j++) {
+				
+				if (board[j][num] != 0) {
+					now = board[j][num];
+					System.out.println("now : "+now);
+					board[j][num] = 0;
 					break;
-				else {
-					now = a[num].pop();
-					if (now != 0)
-						break;
 				}
+				
 			}
-
-			if (now != 0) {
+			}catch(ArrayIndexOutOfBoundsException e) {
+				break;}
+			
+			
+			
+			if (now != -1) {
 				if (!(b.isEmpty())) {
 					if (now == b.peek()) {
 						b.pop();
-						answer +=2;
-					}
-					else {
+						answer += 2;
+					} else {
 						b.push(now);
 					}
-				}
-				else {
-				b.push(now);
+				} else {
+					b.push(now);
 				}
 
 			}
-		}
-		return answer;
+
+		}return answer;
+
 	}
 
 }
