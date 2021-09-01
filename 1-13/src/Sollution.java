ab8be233044326289e7190317e9a7e8b016b2af6
Sarah <ysarah0503@gmail.com>
2021-09-02 04:03:46 +0900

    [UPDATE]1-13
    4주차
     - 미완성

--- 1-13/src/Sollution.java
+++ 1-13/src/Sollution.java
@@ -1,35 +1,48 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 class Solution {
     public String solution(String[] tables, String[] languages, int[] preference) {
         String answer = "";
-        String [] work = {"SI","CONTENTS","HARDWARE","PORTAL","GAME"};
-        ArrayList  <String> SI = new ArrayList <String>();
-        ArrayList  <String> CONTENTS = new ArrayList <String>();
-        ArrayList  <String> HARDWARE = new ArrayList <String>();
-        ArrayList  <String> PORTAL = new ArrayList <String>();
-        ArrayList  <String> GAME = new ArrayList <String>();
+       // String [] work = {"SI","CONTENTS","HARDWARE","PORTAL","GAME"};
+//        ArrayList  <String> SI = new ArrayList <String>();
+//        ArrayList  <String> CONTENTS = new ArrayList <String>();
+//        ArrayList  <String> HARDWARE = new ArrayList <String>();
+//        ArrayList  <String> PORTAL = new ArrayList <String>();
+//        ArrayList  <String> GAME = new ArrayList <String>();
+        int length = languages.length;
+        HashMap <String, Integer> language = new HashMap <String, Integer>();
+        HashMap <String, Integer> score = new HashMap <String, Integer>();
+        //ArrayList  <String> []  lang = null ;
+        for(int i = 0; i<5; i++) {
+        	for(int j = 1; j<6; j++) {
+        		if(score.containsKey(tables[i].split(" ")[j])) {
+        			score.replace(tables[i].split(" ")[j], score.get(tables[i].split(" ")[j]) + (6-j));
+        		} 
+        		else {
+        			score.put(tables[i].split(" ")[j], 6-j);
+        		}
+        	}
+        }
+        
+        for(int i = 0 ;i<length ; i++) {
+        	int leng_score = 0;
+        	if(score.containsKey(languages[i])) {
+        		leng_score = score.get(languages[i]);
+        	}
+        	//leng_score += preference[i];
+        	language.put(languages[i], leng_score);
+        }
+        
+        
+        Iterator <String> iter = language.keySet().iterator();
+        while(iter.hasNext()) {
+        	String key = iter.next();
+        	Integer value = (Integer)language.get(key);
+        	System.out.println(key + " " + value);
+        }
         // 점수 입력
-        for(int i = 1; i<6;i ++) {
-        	SI.add(tables[0].split(" ")[i]);
-        	System.out.println(tables[0].split(" ")[i]);
-        }
-        for(int i = 1; i<6;i ++) {
-        	CONTENTS.add(tables[0].split(" ")[i]);
-        	System.out.println(tables[1].split(" ")[i]);
-        }
-        for(int i = 1; i<6;i ++) {
-        	HARDWARE.add(tables[0].split(" ")[i]);
-        	System.out.println(tables[2].split(" ")[i]);
-        }
-        for(int i = 1; i<6;i ++) {
-        	PORTAL.add(tables[0].split(" ")[i]);
-        	System.out.println(tables[3].split(" ")[i]);
-        }
-        for(int i = 1; i<6;i ++) {
-        	GAME.add(tables[0].split(" ")[i]);
-        	System.out.println(tables[4].split(" ")[i]);
-        }
         
         return answer;
     }
