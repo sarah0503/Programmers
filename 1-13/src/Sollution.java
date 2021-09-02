65e6ff54884b971da42d1988b4c3cf1a5c6ad854
Sarah <ysarah0503@gmail.com>
2021-09-03 07:12:02 +0900

    [UPDATE]1-13
    4주차
     - 순위만 넣으면 됨

--- 1-13/src/Sollution.java
+++ 1-13/src/Sollution.java
@@ -5,7 +5,6 @@ import java.util.Iterator;
 class Solution {
     public String solution(String[] tables, String[] languages, int[] preference) {
         String answer = "";
-       // String [] work = {"SI","CONTENTS","HARDWARE","PORTAL","GAME"};
         HashMap <String, Integer> SI = new  HashMap <String, Integer>();
         HashMap <String, Integer> CONTENTS = new  HashMap <String, Integer>();
         HashMap <String, Integer> HARDWARE = new  HashMap <String, Integer>();
@@ -15,17 +14,7 @@ class Solution {
         HashMap <String, Integer> language = new HashMap <String, Integer>();
         HashMap <String, Integer> score = new HashMap <String, Integer>();
         HashMap <String, Integer> pre = new HashMap <String, Integer>();
-        //ArrayList  <String> []  lang = null ;
-//        for(int i = 0; i<5; i++) {
-//        	for(int j = 1; j<6; j++) {
-//        		if(score.containsKey(tables[i].split(" ")[j])) {
-//        			score.replace(tables[i].split(" ")[j], score.get(tables[i].split(" ")[j]) + (6-j));
-//        		} 
-//        		else {
-//        			score.put(tables[i].split(" ")[j], 6-j);
-//        		}
-//        	}
-//        }
+
         //선호도 입력
         for(int i = 0; i<length; i++) {
         	pre.put(languages[i], preference[i]);
@@ -33,48 +22,18 @@ class Solution {
         //점수 계산
         for(int j = 1; j<6; j++) {
         	SI.put(tables[0].split(" ")[j], 6-j);
-//    		if(score.containsKey(tables[0].split(" ")[j])) {
-//    			score.replace(tables[0].split(" ")[j], score.get(tables[0].split(" ")[j]) + (6-j));
-//    		} 
-//    		else {
-//    			score.put(tables[0].split(" ")[j], 6-j);
-//    		}
         }
         for(int j = 1; j<6; j++) {
         	CONTENTS.put(tables[1].split(" ")[j], 6-j);
-//    		if(score.containsKey(tables[1].split(" ")[j])) {
-//    			score.replace(tables[1].split(" ")[j], score.get(tables[1].split(" ")[j]) + (6-j));
-//    		} 
-//    		else {
-//    			score.put(tables[1].split(" ")[j], 6-j);
-//    		}
         }
         for(int j = 1; j<6; j++) {
         	HARDWARE.put(tables[2].split(" ")[j], 6-j);
-//    		if(score.containsKey(tables[2].split(" ")[j])) {
-//    			score.replace(tables[2].split(" ")[j], score.get(tables[2].split(" ")[j]) + (6-j));
-//    		} 
-//    		else {
-//    			score.put(tables[2].split(" ")[j], 6-j);
-//    		}
         }
         for(int j = 1; j<6; j++) {
         	PORTAL.put(tables[3].split(" ")[j], 6-j);
-//    		if(score.containsKey(tables[3].split(" ")[j])) {
-//    			score.replace(tables[3].split(" ")[j], score.get(tables[3].split(" ")[j]) + (6-j));
-//    		} 
-//    		else {
-//    			score.put(tables[3].split(" ")[j], 6-j);
-//    		}
         }
         for(int j = 1; j<6; j++) {
          	GAME.put(tables[0].split(" ")[j], 6-j);
-//    		if(score.containsKey(tables[4].split(" ")[j])) {
-//    			score.replace(tables[4].split(" ")[j], score.get(tables[4].split(" ")[j]) + (6-j));
-//    		} 
-//    		else {
-//    			score.put(tables[4].split(" ")[j], 6-j);
-//    		}
         }
         
         //언어 넣기
@@ -98,9 +57,9 @@ class Solution {
         			si += SI.get(languages[i]) * pre.get(languages[i]);
         		}
         	}
-        	Integer value = (Integer)SI.get(key);
-        	System.out.println(key + " " + value);
-        	System.out.println(si);
+//        	Integer value = (Integer)SI.get(key);
+//        	System.out.println(key + " " + value);
+//        	System.out.println(si);
         }
         iter = CONTENTS.keySet().iterator();
         while(iter.hasNext()) {
