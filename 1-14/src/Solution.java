5112e9d3a326104d2be21c9a35bbbc9ab4d77307
Sarah <ysarah0503@gmail.com>
2021-09-06 14:20:40 +0900

    [UPDATE]1-14
    폰켓몬

--- 1-14/src/Solution.java
+++ 1-14/src/Solution.java
@@ -1,4 +1,15 @@
-
-public class Solution {
-
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet <Integer> Nums = new HashSet <Integer> ();
+        for (int i = 0; i<nums.length ; i++) {
+        	Nums.add(nums[i]);
+        }
+        //Collections.sort(Nums);
+        int select = nums.length/2;
+        int count = Nums.size();
+        answer = (select<count? select : count);
+        return answer;
+    }
 }
