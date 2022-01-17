60bd427e876734bb4d421d96366256d7033750b8
Sarah <ysarah0503@gmail.com>
2022-01-18 01:07:01 +0900

    [CREATE] 1-20

---
+++ 1-20/src/Solution.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i<numbers.length-1 ; i++) {
+        	for(int j = i+1; j<numbers.length; j++) {
+        		set.add(numbers[i] + numbers[j]);
+        		//System.out.println(numbers[i]+numbers[j]);
+        	}
+        }
+        ArrayList<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        
+        int[] answer = new int[list.size()];
+        int size = 0;
+        for(Integer temp : list) {
+        	answer[size++] = temp;
+        }
+        
+        
+        
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+		int[] numbers = {2,1,3,4,1};
+		Solution s = new Solution();
+		int [] result = s.solution(numbers);
+		System.out.println(result);
+	}
+}
