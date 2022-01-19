46851a53a4624e64b54e45f293226f477f48076d
Sarah <ysarah0503@gmail.com>
2022-01-19 16:55:24 +0900

    [UPDATE] 1-19

--- 1-19/src/Solution.java
+++ 1-19/src/Solution.java
@@ -20,38 +20,13 @@ public class Solution {
         //배열정렬
         Arrays.sort(d);
         
-//        if(budget < total) {
-//        	System.out.println(maxCount + " " + total);
-//        	Sub(d);
-//        	System.out.println(maxCount + " " + total );
-//        }
-        
-//        while(true) {
-//        	Check(budget);
-//        	if(check == false) {
-//        		Sub(d);
-//        	}
-//        	else {
-//        		return maxCount +1;
-//        	}
-//        }
-        
         while(check == false) {
-        	//if(check == false) {
         		Sub(d);
-        	//}
-        	//else {
-        		//return maxCount +1;
-        	//}
             	Check(budget);
         }
         
         return maxCount +1;
     }
-    
-//    public boolean check(int total, int budget, int[] target) {
-//    	
-//    }
     
 	public void Sub ( int[] target) {
     	total -= target[maxCount];
@@ -69,10 +44,8 @@ public class Solution {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution s = new Solution();
-//		int[] d = {1,3,2,5,4};
-//		int budget = 9;
-		int[] d = {2,2,2,3};
-		int budget = 10;
+		int[] d = {1,3,2,5,4};
+		int budget = 9;
 		System.out.println(
 				s.solution(d, budget));
 	}
