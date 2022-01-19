513eed0eb342e3ee416b2ddde2296ee1c4c433e1
Sarah <ysarah0503@gmail.com>
2022-01-20 00:02:56 +0900

    [UPDATE] 1-25

--- 1-25/src/Solution.java
+++ 1-25/src/Solution.java
@@ -2,26 +2,64 @@
 public class Solution {
 	
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
-        int[] arr1Bin = new int[arr1.length];
-        int [] arr2Bin = new int [arr2.length];
+        String[] answer = new String [n];
+        long maxBin = (2^n) -1;
+        int[][] arr1Bin = new int[n][n];
+        int [][] arr2Bin = new int [n][n];
         
         //이진법으로 변환
         for(int i = 0; i < arr1.length; i++) {
-        	
+        	int a1 = arr1[i];
+        	int a2 = arr2[i];
+        	System.out.println();
+        	for(int j = n-1; j >= 0; j--) {
+        		int b = (int) Math.pow(2, j);
+        		if(a1 >= b) {
+        			a1 -= b;
+        			arr1Bin[i][n-j-1] = 1;
+        		}
+        		else{
+        			arr1Bin[i][n-j-1] = 0;
+        		}
+        		
+
+        		if(a2 >= b) {
+        			a2 -= b;
+        			arr2Bin[i][n-j-1] = 1;
+        		}
+        		else{
+        			arr2Bin[i][n-j-1] = 0;
+        		}
+        	}
+        }
+        
+        for (int i = 0; i<n;i ++) {
+        	answer[i] = "";
+        	for(int j = 0; j<n; j++) {
+        		if(arr1Bin[i][j] == 1|| arr2Bin[i][j] == 1) {
+        			//answer[i].concat("#");
+        			answer[i] = answer[i] + "#";
+        			System.out.println("중");
+        		}
+        		else {
+        			//answer[i].concat(" ");
+        			answer[i] = answer[i] + " ";
+        			System.out.println("뿡");
+        		}
+        	}
         }
         return answer;
     }
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int n = 6;
-//		int [] arr1 = {46,33,33,22,31,50};
-//		int [] arr2 = {27,56,19,14,14,10};
+		int n = 6;
+		int [] arr1 = {46,33,33,22,31,50};
+		int [] arr2 = {27,56,19,14,14,10};
 		
-		int n = 5;
-		int [] arr1 = {9,20,28,18,11};
-		int [] arr2 = {30,1,21,17,28};
+//		int n = 5;
+//		int [] arr1 = {9,20,28,18,11};
+//		int [] arr2 = {30,1,21,17,28};
 		
 		Solution s = new Solution();
 		String [] answer = s.solution(n, arr1, arr2);
