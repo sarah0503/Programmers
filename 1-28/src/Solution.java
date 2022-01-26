2ea9bcf9a51dd00e88995320d0fa4d5443289a08
Sarah <ysarah0503@gmail.com>
2022-01-26 16:44:41 +0900

    [UPDATE] 1-28

--- 1-28/src/Solution.java
+++ 1-28/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Solution {
 
@@ -5,20 +7,30 @@ public class Solution {
 		int answer = 0;
 		int answers[] = new int[3];
 		String[] results = dartResult.split("[0-9]");
+		ArrayList <String> resultList = new ArrayList <String>();
+		
+		for(int i = 0; i<results.length ;i++) {
+			if(!results[i].equals("")) {
+				resultList.add(results[i]);
+			}
+		}
 		//String dartNums = dartResult.replaceAll("[^0-9]", "");
 		String[] dartNums = dartResult.split("[A-Z]");
 		for(int i = 0; i<dartNums.length; i++) {
 			dartNums[i] = dartNums[i].replaceAll("[^0-9]", "");
 		}
 		
-		for(String s : results) {
-			System.out.println("결과 " + s);
-		}
+		Iterator iter = resultList.iterator();
+		
+//		while(iter.hasNext()) {
+//			System.out.println("결과 " + iter.next());
+//		}
 
 		// for(String s: results) {
 		for (int i = 0; i < 3; i++) {
-			String s = results[i + 1];
-			System.out.println(s + " ㅋ");
+			//String s = results[i + 1];
+			String s = (String) iter.next();
+			//System.out.println(s + " ㅋ");
 			answers[i] = square(Character.toString(s.charAt(0)),
 					Integer.parseInt((dartNums[i]
 							)));
@@ -48,19 +60,18 @@ public class Solution {
 		// TODO Auto-generated method stub
 		String dartResult = "1D2S#10S";
 		Solution s = new Solution();
-		System.out.println();
 		System.out.println(s.solution(dartResult));
 	}
 
 	public int square(String s, int num) {
 		if (s.equals("S")) {
-			System.out.println(num);
+			//System.out.println(num);
 			return num;
 		} else if (s.equals("D")) {
-			System.out.println(num * num);
+			//System.out.println(num * num);
 			return num * num;
 		} else {
-			System.out.println(num * num * num);
+			//System.out.println(num * num * num);
 			return num * num * num;
 		}
 
