caa68543a9890eab899ef0d893ba2c61415157de
Sarah <ysarah0503@gmail.com>
2022-02-03 02:05:08 +0900

    [UPDATE] 1-29

--- 1-29/src/Solution.java
+++ 1-29/src/Solution.java
@@ -2,31 +2,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 public class Solution {
 	   public int[] solution(int [] arr) {
 	        int [] answer = {};
-	        //List<Integer> arrayList = Arrays.asList(arr);
-	        
-	        
-	        //ArrayList <Integer> arrayList = (ArrayList<Integer>) (Arrays.asList(arr));
-	        Set<int[]> set = new HashSet<> (Arrays.asList(arr));
-	        	
-	        Iterator<int[]> it = set.iterator();
-	        
-//	        int i = 0;
-//	        while(it.hasNext()) {
-//	        	answer[i] = it.next();
-//	        	System.out.println(it.next());
-//	        	i++;
-//	        }
-	        
-	        Integer[] array = set.toArray(new Integer[0]);
-	        
-	        System.out.println(Arrays.toString(array));
-	       
+	        LinkedList <Integer> list = new LinkedList<Integer>();
+	        list.add(arr[0]);
+	        for (int i = 1; i<arr.length;i++) {
+	        	if(arr[i]!= list.getLast()) {
+	        		list.add(arr[i]);
+	        	}
+	        }
+	        Integer[] intArray = list.toArray(new Integer [list.size()]);
+	        answer = Arrays.stream(intArray).mapToInt(Integer::intValue).toArray();
 	        return answer;
 	    }
 	   
@@ -35,8 +26,5 @@ public class Solution {
 		   Solution s = new Solution();
 		   int [] arr = {1,1,3,3,0,1,1};
 		  int[] newArr = s.solution(arr);
-		  for(int i = 0; i<newArr.length; i++) {
-			  System.out.println(newArr[i]);
-		  }
 	}
 }
