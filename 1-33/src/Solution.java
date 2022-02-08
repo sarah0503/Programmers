e8f5a7a15ce0f2fdd88cd461bc79e21dafdac890
Sarah <ysarah0503@gmail.com>
2022-02-09 02:09:11 +0900

    [CREATE] 1-33

---
+++ 1-33/src/Solution.java
@@ -0,0 +1,23 @@
+
+public class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        s = s.toLowerCase();
+        int pCount = 0; 
+        int yCount = 0;
+        for(int i = 0 ;i<s.length(); i++) {
+        	if(s.charAt(i) == ('p')) {
+        		pCount ++;
+        	}
+        	else if(s.charAt(i) == ('y')) {
+        		yCount ++;
+        	}
+        }
+        
+        if(pCount != yCount ) {
+        	answer = false;
+        }
+
+        return answer;
+    }
+}
