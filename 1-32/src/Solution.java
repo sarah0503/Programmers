a7b6b388b0fdae95a3e2840fda0189016de32e50
Sarah <ysarah0503@gmail.com>
2022-02-08 22:50:16 +0900

    [UPDATE] 1-32

--- 1-32/src/Solution.java
+++ 1-32/src/Solution.java
@@ -1,13 +1,44 @@
+import java.util.Arrays;
 
 public class Solution {
     public String[] solution(String[] strings, int n) {
         String[] answer = {};
-        int [] ranking = new int [answer.length];
-        String [] chracter = new String [answer.length];
-        int max = 0;
+        int [] ranking = new int [strings.length];
+        Arrays.sort(strings);
+       // Arrays.fill(ranking, -1);
         
-        for(int i = 0; i<answer.length; i++) {
-        	
+//        for(int i  = 0; i<ranking.length; i++) {
+//        	System.out.println(ranking[i]);
+//        }
+        
+        
+        String [] chracter = new String [strings.length];
+        int max;
+   
+        
+        for(int i = 0; i<strings.length; i++) {
+        	if (ranking[i] == 0) {
+        		//max =  String.valueOf(strings[i].charAt(n));
+        		//System.out.println("Q");
+        		max = i;
+        		for(int j = i+1; j<strings.length;j++) {
+        			//System.out.println(j);
+        			if(ranking[j] == 0) {
+        			int compare = String.valueOf(strings[max].charAt(n)).compareTo( String.valueOf(strings[j].charAt(n)));
+        			if(compare < 0) {
+        				max = j;
+        			}
+        			}
+        		}
+        		ranking[max] = strings.length - i;
+        		
+        	}else {
+        		break;
+        	}
+        }
+        
+        for(int i = 0; i<strings.length;i++) {
+        	System.out.println(ranking[i]);
         }
         return answer;
     }
@@ -17,7 +48,10 @@ public class Solution {
 		Solution s = new Solution();
 		String [] strings = {"abcd", "abce", "cdx"};
 		int n = 2;
-		s.solution(strings, n);
+		String [] answer = s.solution(strings, n);
+		for(int i = 0; i<answer.length; i++) {
+			System.out.println(answer[i]);
+		}
 		
 	}
 }
