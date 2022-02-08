f1ba721318176f09c712e1e1ae536767b66ddf9a
Sarah <ysarah0503@gmail.com>
2022-02-08 23:15:06 +0900

    [UPDATE] 1-32

--- 1-32/src/Solution.java
+++ 1-32/src/Solution.java
@@ -3,50 +3,38 @@ import java.util.Arrays;
 public class Solution {
     public String[] solution(String[] strings, int n) {
         String[] answer = {};
-        int [] ranking = new int [strings.length];
-        Arrays.sort(strings);
-       // Arrays.fill(ranking, -1);
-        
-//        for(int i  = 0; i<ranking.length; i++) {
-//        	System.out.println(ranking[i]);
-//        }
-        
-        
-        String [] chracter = new String [strings.length];
-        int max;
-   
+  
+        String temp;
         
         for(int i = 0; i<strings.length; i++) {
-        	if (ranking[i] == 0) {
-        		//max =  String.valueOf(strings[i].charAt(n));
-        		//System.out.println("Q");
-        		max = i;
-        		for(int j = i+1; j<strings.length;j++) {
-        			//System.out.println(j);
-        			if(ranking[j] == 0) {
-        			int compare = String.valueOf(strings[max].charAt(n)).compareTo( String.valueOf(strings[j].charAt(n)));
-        			if(compare < 0) {
-        				max = j;
-        			}
+        	for(int j = i+1; j<strings.length ; j++) {
+        		int compare = (String.valueOf(strings[i].charAt(n))).compareTo( String.valueOf(strings[j].charAt(n)));
+        		if(compare > 0) {
+        			temp = strings[i];
+        			strings[i] = strings[j];
+        			strings[j] = temp;
+        		}
+        		else if( compare == 0) {
+        			if(strings[i].compareTo(strings[j]) < 0) {
+
+            			temp = strings[i];
+            			strings[i] = strings[j];
+            			strings[j] = temp;
         			}
         		}
-        		ranking[max] = strings.length - i;
-        		
-        	}else {
-        		break;
         	}
         }
         
-        for(int i = 0; i<strings.length;i++) {
-        	System.out.println(ranking[i]);
-        }
+        answer = strings;
+       
+        
         return answer;
     }
     
     
     public static void main(String[] args) {
 		Solution s = new Solution();
-		String [] strings = {"abcd", "abce", "cdx"};
+		String [] strings = { "abce","abcd", "cdx"};
 		int n = 2;
 		String [] answer = s.solution(strings, n);
 		for(int i = 0; i<answer.length; i++) {
