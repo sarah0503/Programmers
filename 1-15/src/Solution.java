0a01b4bb7cee8e8c2106e46c7fb979bf28e91a9f
Sarah <ysarah0503@gmail.com>
2022-02-17 01:41:38 +0900

    [UPDATE] 1-15

--- 1-15/src/Solution.java
+++ 1-15/src/Solution.java
@@ -2,92 +2,66 @@
 import java.util.*;
 
 class Solution {
-    public ArrayList<Integer> solution(int N, int[] stages) {
-        ArrayList<Integer> answer = new ArrayList<Integer>();
-        int people = stages.length;
-        //TreeMap<Integer, Double> fail = new TreeMap <Integer, Double>();
-        ArrayList <Double> fail = new ArrayList <Double>();
-    	double ch_count = people;
-        for(int i =0; i<N+1;i++) {
-        	double count = 0;
-        	double fail_count = 0;
-        	for(int j = 0; j<people; j++) {
-        		if (stages[j] == (i+1))
-        			fail_count ++;
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int [N];
+        double [] fail_percentage = new double [N];
+        for(int i = 1; i<=N; i++) {
+            double fail = 0;
+            double come = 0;
+        	for(int j = 0; j<stages.length; j++) {
+        		
+        		if(i <= stages[j]) {
+        			come ++;
+        		}
+        		 if( i == stages[j]) {
+        			fail ++;
+        		}
         	}
-        	//System.out.println(fail_count);
-//        	if(ch_count < 0.0) {
-//        		count = 0;
-//        	}
-//        	else {
-        	count = (fail_count) / ch_count;
-        	//System.out.println(ch_count);
-        	//System.out.println(count);
-            //System.out.println(fail_count + " " + ch_count);
-        	ch_count -= fail_count;
-//        	}
-        	//fail.put(i+1, count); 
-        	fail.add(count);
+        	fail_percentage[i-1] = fail/come;
+        	System.out.println(fail_percentage[i-1]);
+        	answer[i-1] = i;
         }
         
-       //Iterator <Integer> it = fail.descendingKeySet().iterator();
-        
-//       Iterator <Double> it = fail.descendingKeySet().iterator();
-//       int i = 0;
-//       while(it.hasNext()) {
-//    	   Double key = it.next();
-//    	   System.out.println(fail.get(key));
-//    	   System.out.println(key);
-//       }
-        ArrayList temp = new ArrayList <Double>();
-        for(int i = 0; i<fail.size(); i++) {
-        	temp.add(fail.get(i));
-        }
-        temp.sort(Comparator.reverseOrder());
-        for(int i = 0; i<fail.size(); i++) {
-        }
-        System.out.println();
-        for(int i = 0; i<fail.size(); i++) {
-        }
-        boolean check = false ;
-        for(int i = 0; i<fail.size(); i++) {
-        	for(int j = 0; j<fail.size();j++) {
-        		if (temp.get(i).equals(fail.get(j))) {
+        for(int i = 0; i<answer.length; i++) {
+        	for(int j = i+1; j<answer.length; j++) {
+        		if(fail_percentage[i] < fail_percentage[j]) {
+        			double temp = fail_percentage[i];
+        			fail_percentage[i] = fail_percentage[j];
+        			fail_percentage[j] = temp;
         			
-        			for(int k = 0; k< answer.size();k++) {
-        				if(answer.get(k) == j+1) {
-        					check = true;
-        				}
-        			}
-        			if(check == false) {
-        				answer.add(j+1);
-        				break;
-        			}
-        			else {
-        				check = false;
+        			int answer_temp = answer[i];
+        			answer[i] = answer[j];
+        			answer[j] = answer_temp;
+        		}
+        		
+        		if(fail_percentage[i] == fail_percentage[j]) {
+        			if(answer[i] < answer[j]) {
+        				double temp = fail_percentage[i];
+            			fail_percentage[i] = fail_percentage[j];
+            			fail_percentage[j] = temp;
+            			
+            			int answer_temp = answer[i];
+            			answer[i] = answer[j];
+            			answer[j] = answer_temp;
         			}
         		}
         	}
         }
         
-        for(int i = 0; i<answer.size()-1;i++) {
-        	if (answer.get(i) == N+1) {
-        		answer.remove(i);
-        	}
-        	//System.out.println(answer.get(i));
-        }
-        
-        for (int i = 0; i<answer.size()-1 ; i++) {
-        	System.out.println(answer.get(i));
-        }
         return answer;
     }
     
     public static void main(String[] args) {
     	Solution s = new Solution();
 //    	int [] array = {2,1,2,6,2,4,3,3};
-//		s.solution(5, array);
+//		int[] answer = s.solution(5, array);
+		
     	int [] array = {1,2,2,1,3};
- 		s.solution(8, array);
+ 		int [] answer = s.solution(8, array);
+		
+
+		for(int i = 0; i<answer.length; i++) {
+			System.out.println(answer[i]);
+		}
 	}
 }
