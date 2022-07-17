d19ba57c0e9a1fcf7cc2d410eee042491cbb9acf
Sarah <ysarah0503@gmail.com>
2022-07-18 03:10:36 +0900

    [UPDATE] 2-7

--- 2-7/src/Solution.java
+++ 2-7/src/Solution.java
@@ -1,42 +1,25 @@
+import java.util.Stack;
 
 public class Solution {
 
 	public int solution(String s) {
-		int answer = -1;
+		int answer = 0;
 
-		String str = s;
-		String newStr = "";
-		int count = 0;
+		Stack<Character> stack = new Stack<>();
 
-		System.out.println(str);
-		while (true) {
-			str = delStr(str);
-			System.out.println(str);
-			if (str == "")
-				return 1;
-			if (str.equals("-1"))
-				return 0;
-
-			if (count == -1)
-				break;
-		}
-		return answer;
-	}
-
-	String delStr(String str) {
-		String string = "-1";
-		for (int i = 0; i < str.length() - 1; i++) {
-			if (str.charAt(i) == str.charAt(i + 1)) {
-				if (str.length() == 0) {
-					string = "";
-					break;
-				}
-				string = str.substring(0, i) + str.substring(i + 2);
-				break;
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (!stack.isEmpty() && stack.peek() == c) {
+				stack.pop();
+			} else {
+				stack.push(c);
 			}
-
 		}
-		return string;
+
+		if (stack.size() == 0)
+			return 1;
+
+		return answer;
 	}
 
 	public static void main(String[] args) {
