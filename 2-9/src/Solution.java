634e43865677d63a4acc5d023e0e2f5f11650464
sarah0503 <ysarah0503@gmail.com>
2022-07-19 17:08:06 +0900

    [UPDATE] 2-9

--- 2-9/src/Solution.java
+++ 2-9/src/Solution.java
@@ -4,9 +4,9 @@ public class Solution {
         int[] answer = {};
         int[][] numbers = new int[columns][rows];
         
-        for(int i = 0; i< columns; i++) {
-        	for(int j = 0; j<rows; j++) {
-        		numbers[i][j] = (i*rows) + j +1;
+        for(int i = 0; i< rows; i++) {
+        	for(int j = 0; j<columns; j++) {
+        		numbers[j][i] = (i*columns) + j +1;
         	}
         }
         
@@ -20,64 +20,61 @@ public class Solution {
             int temp1 = 0;
             int temp2 = 0;
             
-        	temp1 = numbers[queries[i][0] - 1][queries[i][1] - 1 ];
-            System.out.println();
+        	temp1 = numbers[queries[i][1] - 1 ][queries[i][0] - 1];
         	
         	if(temp1 <= min) min = temp1;
-            
-            for (int x = 1; x < k+1; x ++) {
-            	temp2 = numbers[queries[i][0] - 1][queries[i][1] - 1 + (x)];
-            	numbers[queries[i][0] ][queries[i][1] - 1 + (x)] = temp1;
-            	if(temp1 <= min) min = temp1;
-            	temp1 = temp2;
-            }
-            
-            
-            for (int y = 1; y<z+1; y++) {
-            	temp2 = numbers[queries[i][0] - 1 +(y)][queries[i][3] - 1 ];
-            	numbers[queries[i][0]- 1 +(y)][queries[i][3] - 1 ] = temp1;
-            	if(temp1 <= min) min = temp1;
-            	temp1 = temp2;
+         
+        	for(int x = 1; x<=k; x++) {
+        		temp2 = numbers[queries[i][1] -1 + (x)][queries[i][0] -1 ];
+        		numbers[queries[i][1] -1 + (x)][queries[i][0] -1 ]  = temp1;
+        		temp1 = temp2;
+        		if(temp1 <= min) min = temp1;
+        	}
+        	
+        	for(int x =1; x<=z; x++) {
+        		temp2 = numbers[queries[i][3]-1][queries[i][0] -1 +(x)];
+        		numbers[queries[i][3]-1][queries[i][0] -1 +(x)] = temp1;
+        		temp1 = temp2;
+        		if(temp1 <= min) min = temp1;
+        	}
+        	
+        	for(int x = 1; x<=k; x++) {
+        		temp2 = numbers[queries[i][1] -1 + (k-x)][queries[i][2] -1 ];
+        		numbers[queries[i][1] -1 + (k-x)][queries[i][2] -1 ]  = temp1;
+        		temp1 = temp2;
+        		if(temp1 <= min) min = temp1;
+        	}
+        	
+        	for(int x =1; x<=z; x++) {
+        		temp2 = numbers[queries[i][1]-1][queries[i][0] -1 +(z-x)];
+        		numbers[queries[i][1]-1][queries[i][0] -1 +(z-x)] = temp1;
+        		temp1 = temp2;
+        		if(temp1 <= min) min = temp1;
+        	}
+        	
+        
             			
-            }
-            
-            for (int x = 1; x < k+1; x ++) {
-            	temp1 = numbers[queries[i][2]- 1][queries[i][1]- 1 + (k-x)];
-            	numbers[queries[i][2]- 1][queries[i][1] - 1 + (k-x)] = temp1;
-            	if(temp1 <= min) min = temp1;
-            	temp1 = temp2;
-            }
-            
-            for (int y = 1; y<z+1; y++) {
-            	temp1 = numbers[queries[i][0]- 1 +(z-y)][queries[i][1] - 1] ;
-            	numbers[queries[i][0] - 1 +(z-y)][queries[i][1] - 1 ] = temp1;
-            	if(temp1 <= min) min = temp1;
-            	temp1 = temp2;
-            			
-           
+            System.out.println("min = " + min);
         }
 
-            System.out.println("KKKKKKK");
-            
-            for(int q = 0; q< columns; q++) {
-            	for(int j = 0; j<rows; j++) {
-            		System.out.println(numbers[q][j]);
-            	}
-            	System.out.println();
-            }
-
-            
-            System.out.println("min " + min);
-        }
+          
         return answer;
         
     }
 
     public static void main(String[] args) {
 		Solution s = new Solution();
-		int rows = 6;
-		int columns = 6;
-		int [][] queries = {{2,2,5,4}, {3,3,6,6}, {5,1,6,3}};
+//		int rows = 6;
+//		int columns = 6;
+//		int [][] queries = {{2,2,5,4}, {3,3,6,6}, {5,1,6,3}};
+		
+//		int rows = 3;
+//		int columns = 3;
+//		int [][] queries = {{1,1,2,2}, {1,2,2,3},{2,1,3,2}};
+		
+		int rows = 100; 
+		int columns = 97;
+		int[][] queries = {{1,1,100,97}};
 		
 		s.solution(rows, columns, queries);
 	}
