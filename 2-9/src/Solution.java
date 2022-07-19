45f1164e8bc751f9d77513a5c9b15e1539c01499
sarah0503 <ysarah0503@gmail.com>
2022-07-19 16:14:42 +0900

    [CREATE] 2-9
    1차

--- 2-9/src/Solution.java
+++ 2-9/src/Solution.java
@@ -10,17 +10,73 @@ public class Solution {
         	}
         }
         
-        for(int i = 0; i<queries.length; i++) {
-        	System.out.println("s");
-        }
+
         
+        for(int i = 0; i<queries.length; i++) {
+            int min = rows  * columns;
+            int k = queries[i][3] - queries[i][1];
+            int z = queries[i][2] - queries[i][0];
+
+            int temp1 = 0;
+            int temp2 = 0;
+            
+        	temp1 = numbers[queries[i][0] - 1][queries[i][1] - 1 ];
+            System.out.println();
+        	
+        	if(temp1 <= min) min = temp1;
+            
+            for (int x = 1; x < k+1; x ++) {
+            	temp2 = numbers[queries[i][0] - 1][queries[i][1] - 1 + (x)];
+            	numbers[queries[i][0] ][queries[i][1] - 1 + (x)] = temp1;
+            	if(temp1 <= min) min = temp1;
+            	temp1 = temp2;
+            }
+            
+            
+            for (int y = 1; y<z+1; y++) {
+            	temp2 = numbers[queries[i][0] - 1 +(y)][queries[i][3] - 1 ];
+            	numbers[queries[i][0]- 1 +(y)][queries[i][3] - 1 ] = temp1;
+            	if(temp1 <= min) min = temp1;
+            	temp1 = temp2;
+            			
+            }
+            
+            for (int x = 1; x < k+1; x ++) {
+            	temp1 = numbers[queries[i][2]- 1][queries[i][1]- 1 + (k-x)];
+            	numbers[queries[i][2]- 1][queries[i][1] - 1 + (k-x)] = temp1;
+            	if(temp1 <= min) min = temp1;
+            	temp1 = temp2;
+            }
+            
+            for (int y = 1; y<z+1; y++) {
+            	temp1 = numbers[queries[i][0]- 1 +(z-y)][queries[i][1] - 1] ;
+            	numbers[queries[i][0] - 1 +(z-y)][queries[i][1] - 1 ] = temp1;
+            	if(temp1 <= min) min = temp1;
+            	temp1 = temp2;
+            			
+           
+        }
+
+            System.out.println("KKKKKKK");
+            
+            for(int q = 0; q< columns; q++) {
+            	for(int j = 0; j<rows; j++) {
+            		System.out.println(numbers[q][j]);
+            	}
+            	System.out.println();
+            }
+
+            
+            System.out.println("min " + min);
+        }
         return answer;
+        
     }
 
     public static void main(String[] args) {
 		Solution s = new Solution();
 		int rows = 6;
-		int columns = 5;
+		int columns = 6;
 		int [][] queries = {{2,2,5,4}, {3,3,6,6}, {5,1,6,3}};
 		
 		s.solution(rows, columns, queries);
